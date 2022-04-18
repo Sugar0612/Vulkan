21ca03845dc739e6938d36102f9087aebb02aaf3
Sugar <287276293@qq.com>
2022-04-18 21:18:52 +0800

    2022-04-18 craete Shader

---
+++ vulkanLearning/Shader/compile.bat
@@ -0,0 +1,5 @@
+H:\Project\Vulkan\thirdParty\Vulkan\Bin\glslangValidator.exe  -V vertex.vert -o vs.spv
+
+H:\Project\Vulkan\thirdParty\Vulkan\Bin\glslangValidator.exe  -V fragment.frag -o fs.spv
+
+pause
